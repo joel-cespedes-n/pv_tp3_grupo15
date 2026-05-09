21ef6d2fba468325ad298185a183da57c397108b
Joel <yoel22cii@gmail.com>
2026-05-09 19:26:16 -0300

    feat: Agrego componentes funcionales: Footer, Header, Nav y ListaProyectos y estilos CSS. [Cespedes Joel Normando]

---
+++ src/components/ListaProyectos.jsx
@@ -0,0 +1,9 @@
+const ListaProyectos = () => {
+    return (
+        <div>
+            <h2>Lista de Proyectos</h2>
+        </div>
+    )
+}
+
+export default ListaProyectos;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Perfil</a></li>
+                <li><a href="#">Proyectos</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
