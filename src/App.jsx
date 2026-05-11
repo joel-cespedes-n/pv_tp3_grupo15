8ed46842c9afe67e69aae3a431e859d4487f72a3
Gabriel <gutimaxaxatr@gmail.com>
2026-05-10 22:45:47 -0300

    Actualización de estilos y estructura -  Gutierrez Maximiliano Gabriel

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,23 @@
 import { useState } from 'react'
 import proyectoService from './services/proyectoService.js'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import ListaProyectos from './components/ListaProyectos'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import './css/styles.css'
 
-const App = () => {
+import ListaProyectos from './components/ListaProyectos'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
-  const[proyectos, setProyecto] = useState(proyectoService.obtenerProyectos())
-  const [count, setCount] = useState(0)
-
+  const App = () => {
   return (
     <div id='root'>
       <Header />
-
+      <Nav />
       <main>
-        <ListaProyectos/>
+        <ListaProyectos />
       </main>
-
-       <section id="proyectos">
-        <h2>Lista de Proyectos</h2>
-        <ul>
-          {proyectos.map(p => (
-            <li key={p.id}>
-              {p.titulo} - {p.categoria} ({p.estado})
-            </li>
-          ))}
-        </ul>
-      </section>
-      
       <Footer />
     </div>
   )
 }
-
 export default App
