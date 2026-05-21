d3e72eddffbf6bf5b3cf3b3e4a88812eb0c70652
Joel <yoel22cii@gmail.com>
2026-05-21 20:38:51 -0300

    refactor: Borro comentarios sin alterar el funcionamiento. [Cespedes Joel Normando]

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -9,14 +9,6 @@ const ListaProyectos = () => {
     
     const [proyectos, setProyectos] = useState(proyectoService.obtenerProyectos());
     const [busqueda, setBusqueda] = useState("");
-    /*const [nuevoProyecto, setNuevoProyecto] = useState({
-        titulo: "",
-        categoria: "",
-        estado: "Activo",
-        descripcion: "",
-        recursos: [],
-        equipo: []
-    });*/
     const [proyectoSeleccionado, setProyectoSeleccionado] = useState(null);
 
     const[ultimaActualizacion, setUltimaActualizacion] = useState(null);
@@ -28,21 +20,10 @@ const ListaProyectos = () => {
         ? Math.max(...proyectos.map(p => p.id)) + 1
         : 1;
 
-        /*const proyectoAgregar = {
-            id: nuevoId,
-            titulo: nuevoProyecto.titulo,
-            categoria: nuevoProyecto.categoria,
-            estado: nuevoProyecto.estado,
-            descripcion: nuevoProyecto.descripcion,
-            recursos: nuevoProyecto.recursos,
-            equipo: nuevoProyecto.equipo
-        };*/
         const proyectoAgregar = {...nuevoProyecto, id: nuevoId };
 
         proyectoService.agregarProyecto(proyectoAgregar);
         setProyectos(proyectoService.obtenerProyectos());
-
-        /*setNuevoProyecto({ titulo: "", categoria: "", estado: "Activo", descripcion: "", recursos: [], equipo: [] });*/
      
         const ahora = new Date();
     setUltimaActualizacion(
@@ -57,15 +38,7 @@ const ListaProyectos = () => {
     setUltimaActualizacion(
         `${ahora.getDate()}/${ahora.getMonth() + 1}/${ahora.getFullYear()} a las ${ahora.getHours()}:${ahora.getMinutes()} hs.`
     );
-}
-
-    /*const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNuevoProyecto({
-            ...nuevoProyecto,
-            [name]: value
-        });
-    };*/
+};
 
 const handleVerDetalle = (id) =>{
     const proyecto = proyectoService.obtenerProyectoPorId(id);
@@ -92,108 +65,6 @@ const handleVerDetalle = (id) =>{
             onChange={e => setBusqueda(e.target.value)} 
             />
 
-            {/*
-            <input 
-                type="text" 
-                name="titulo"
-                placeholder="Título" 
-                value={nuevoProyecto.titulo} 
-                onChange={handleInputChange} 
-            />
-            
-            <input 
-                type="text" 
-                name="categoria"
-                placeholder="Categoría" 
-                value={nuevoProyecto.categoria} 
-                onChange={handleInputChange} 
-            />
-            
-            <textarea
-                name="descripcion"
-                placeholder="Descripción" 
-                value={nuevoProyecto.descripcion} 
-                onChange={handleInputChange} 
-            />
-
-           {nuevoProyecto.recursos.map((recurso, index) => (
-            <div key={index}>
-                <input
-                type="text"
-                placeholder={`Recurso ${index + 1}`}
-                value={recurso}
-                onChange={(e) => {
-                    const nuevosRecursos = [...nuevoProyecto.recursos];
-                    nuevosRecursos[index] = e.target.value;
-                    setNuevoProyecto({ ...nuevoProyecto, recursos: nuevosRecursos });
-                }}
-                />
-            </div>
-            ))}
-
-            <button
-            type="button"
-            onClick={() =>
-                setNuevoProyecto({
-                ...nuevoProyecto,
-                recursos: [...nuevoProyecto.recursos, ""]
-                })
-            }
-            >
-            ➕ Agregar recurso
-            </button>
-
-            {nuevoProyecto.equipo.map((integrante, index) => (
-            <div key={index}>
-                <input
-                type="text"
-                placeholder="Nombre"
-                value={integrante.nombre}
-                onChange={(e) => {
-                    const nuevoEquipo = [...nuevoProyecto.equipo];
-                    nuevoEquipo[index].nombre = e.target.value;
-                    setNuevoProyecto({ ...nuevoProyecto, equipo: nuevoEquipo });
-                }}
-                />
-                <input
-                type="text"
-                placeholder="Rol"
-                value={integrante.rol}
-                onChange={(e) => {
-                    const nuevoEquipo = [...nuevoProyecto.equipo];
-                    nuevoEquipo[index].rol = e.target.value;
-                    setNuevoProyecto({ ...nuevoProyecto, equipo: nuevoEquipo });
-                }}
-                />
-            </div>
-            ))}
-
-            <button
-            type="button"
-            onClick={() =>
-                setNuevoProyecto({
-                ...nuevoProyecto,
-                equipo: [...nuevoProyecto.equipo, { nombre: "", rol: "" }]
-                })
-            }
-            >
-            ➕ Agregar integrante
-            </button>
-
-           
-            <select 
-                name="estado"
-                value={nuevoProyecto.estado} 
-                onChange={handleInputChange}
-            >
-                <option value="Activo">Activo</option>
-                <option value="En desarrollo">En desarrollo</option>
-                <option value="Pendiente">Pendiente</option>
-                <option value="Finalizado">Finalizado</option>
-            </select>
-            
-            <button onClick={handleAdd}>Agregar Proyecto</button>
-            */}
             <FormularioProyecto onAgregar={handleAdd} />
 
             <div className="lista">
